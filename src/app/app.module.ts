e2733d377b7d400495264cbc3563fcccd3ba3158
Sridhar Mane <sridhars4s@gmail.com>
2017-04-22 19:56:23 -0400

    Added Search Component, Detail Component and their Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [
-  { path: 'search', component: SearchComponent },
-  { path: '', redirectTo: '/search', pathMatch: 'full'}
-];
+
 
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
-import {RoboAssistantService} from "./roboAssistant.service";
+import { DetailComponent } from './detail/detail.component';
+import { RoboAssistantService } from './roboAssistant.service';
+
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'detail', component: SearchComponent },
+  { path: 'detail/:roboId', component: DetailComponent },
+  { path: '', redirectTo: '/search', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    MaterialModule
   ],
-  providers: [RoboAssistantService, ],
+  providers: [RoboAssistantService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
